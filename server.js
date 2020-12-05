f1b87bbb7ce3c5414debbc59da7123f43dc48ff9
Michael Li <Mli2950@gmail.com>
2020-12-05 11:26:30 -0700

    Express server working

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const path = require('path')
+
+const app = express();
+const PORT = 3000;
+
+const reservations = [
+    {
+        customerName: "tables test"
+    }
+]
+
+const waitList = [
+    {
+        customerName: "reservations test"
+    }
+]
+
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'home.html')))
+app.get('/tables', (req, res) => res.sendFile(path.join(__dirname, 'tables.html')))
+app.get('/reservations', (req, res) => res.sendFile(path.join(__dirname, 'reservation.html')))
+
+app.get('/api/reservations', (req, res) => res.json(reservations))
+app.get('/api/waitlist', (req, res) => res.json(waitList))
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`))
